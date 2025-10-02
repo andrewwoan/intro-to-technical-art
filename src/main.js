512540db6310d80d68eac366e97731ebce7acb97
Andrew Woan <andrewwoan@gmail.com>
2025-10-02 09:57:36 -0400

    starting fragmentshader function

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ import { Fn } from "three/src/nodes/TSL.js";
 // Setup scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  35,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -50,6 +50,8 @@ directionalLight.shadow.mapSize.height = 2048;
 // scene.add(directionalLight);
 
 // Create the custom material once to share between objects
+const rotationAngle = float(Math.PI / 2); // 90 degrees - you can change this value
+
 const fragmentShaderFunction = Fn(() => {
   const finalColor = vec3(positionLocal.x);
 
@@ -69,29 +71,18 @@ gltfLoader.setDRACOLoader(dracoLoader);
 
 // Load the GLB model
 let loadedModel = null;
-gltfLoader.load("/models/Knight.glb", (gltf) => {
+gltfLoader.load("/models/Scene.glb", (gltf) => {
   loadedModel = gltf.scene;
-  loadedModel.position.set(1, 0, 0);
+  loadedModel.position.set(0, 0, 0);
 
   loadedModel.traverse((child) => {
     if (child.isMesh) {
-      child.castShadow = true;
-      child.receiveShadow = true;
-
-      // Apply the shared custom node material to the mesh
       child.material = sharedCustomMaterial;
     }
   });
 
   scene.add(loadedModel);
 });
-
-const planeGeometry = new THREE.PlaneGeometry(2, 2);
-// Use the same custom material instead of MeshLambertMaterial
-const plane = new THREE.Mesh(planeGeometry, sharedCustomMaterial);
-plane.position.x = -2;
-plane.receiveShadow = true;
-scene.add(plane);
 
 const axesHelper = new THREE.AxesHelper(50);
 // scene.add(axesHelper);
@@ -100,7 +91,13 @@ const axesHelper = new THREE.AxesHelper(50);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-camera.position.set(0, 0, 5);
+camera.position.set(8.119320077462113, 3.0038308097924373, 0.686155944870028);
+controls.target.set(
+  -1.4269950171849213,
+  2.1410254465135656,
+  0.07347907348960196
+);
+
 controls.update();
 
 function animate() {
@@ -113,6 +110,9 @@ window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+
+  console.log(camera.position);
+  console.log(controls.target);
 });
 
 // Start the animation loop
